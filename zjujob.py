cbabf95eed2e3c8d04309a36fba51dcffc62c7c0
simplecherry <ryji@mail.ustc.edu.cn>
2016-07-17 23:35:16 +0800

    增加了选择页数的功能

--- zjujob.py
+++ zjujob.py
@@ -5,22 +5,18 @@ from bs4 import BeautifulSoup
 url = 'http://www.career.zju.edu.cn/ejob/zczphxxmorelogin.do'
 front = 'http://www.career.zju.edu.cn/ejob/'
 
-#查询count条记录
-post_data = {
-    'Referer': "http://www.career.zju.edu.cn/ejob/zczphxxmorelogin.do", 
-    'User-Agent':"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0"
-    } 
-return_data = requests.post(url, post_data)
-# print return_data.text
-soup = BeautifulSoup(return_data.text,'lxml')
+#查询2页记录
+#ZJU pagesize为30。
 
-job1=soup.find_all(class_="result_con")[0]
-
-
-for job in job1.find_all(class_="con"):
-    url = front + job.find('a').get('href')
-    name = job.find_all('td')[0].get_text(strip=True)
-    time = " ".join(job.find_all('td')[2].get_text(strip=True).split()) 
-    place = job.find_all('td')[1].get_text(strip=True)
-    print name, place, time
-    print  url,'\n'
+for page in range(1,3):
+    return_data = requests.post(url, data = {'pages.currentPage':str(page)})
+    #print return_data.text
+    soup = BeautifulSoup(return_data.text)
+    for job in soup.find_all(class_="con"):
+        link = front + job.find('a').get('href')
+        #print job.find_all('td')
+        name = job.find_all('td')[0].get_text(strip=True)
+        time = " ".join(job.find_all('td')[2].get_text(strip=True).split()) 
+        place = job.find_all('td')[1].get_text(strip=True)
+        print name, place, time
+        print link,'\n'
