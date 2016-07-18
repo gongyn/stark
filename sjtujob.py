40bb604b2183edbc265559df627e3114793c9b90
yds629 <youdongsen@163.com>
2016-07-18 12:51:32 +0800

    修改post参数，可分别获取不同时段

--- sjtujob.py
+++ sjtujob.py
@@ -2,28 +2,29 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+
  
-url = 'http://www.job.sjtu.edu.cn/eweb/jygl/zpfw.so?type=xjh'
+url = 'http://www.job.sjtu.edu.cn/eweb/jygl/zpfw.so?modcode=jygl_xjhxxck&subsyscode=zpfw&type=searchXjhxx'
 front = 'http://www.job.sjtu.edu.cn/eweb/jygl/zpfw.so?modcode=jygl_xjhxxck&subsyscode=zpfw&type=viewXjhxx&id='
 
 #查询count条记录
 post_data = {
-    'Referer': "http://www.job.sjtu.edu.cn/eweb/jygl/zpfw.so?type=xjh", 
-    'User-Agent':"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0"
+    'xjhType':'yjb',
+    'Referer':'http://www.job.sjtu.edu.cn/eweb/jygl/zpfw.so?modcode=jygl_xjhxxck&subsyscode=zpfw&type=searchXjhxx'
     } 
 return_data = requests.post(url, post_data)
 # print return_data.text
 soup = BeautifulSoup(return_data.text,'lxml')
 
-job1=soup.find_all(id="tbc5_05_4")[0]
+job1=soup.find_all(class_="z_newsl")[0]
 
 pattern = re.compile('\'(.*?)\'',re.S)
 
-for job in job1.find_all(class_="zp_llb"):
+for job in job1.find_all('li')[1:]:
     items = re.findall(pattern,job.find('a').get('onclick'))
-    url = front + items[0]
-    name = job.find_all('li')[0].get_text(strip=True)
-    time = job.find_all('li')[2].get_text(strip=True) + ' ' + job.find_all('li')[3].get_text(strip=True)
-    place = job.find_all('li')[1].get_text(strip=True)
+    link = front + items[0]
+    name = job.find_all('div')[0].get_text(strip=True)
+    time = job.find_all('div')[3].get_text(strip=True) + ' ' + job.find_all('div')[4].get_text(strip=True)
+    place = job.find_all('div')[2].get_text(strip=True)
     print name, place, time
-    print  url,'\n'
+    print  link,'\n'
