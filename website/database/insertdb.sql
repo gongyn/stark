d703bbd61a48f12ed4e6bb1ad62432cb5ff2bf85
yds629 <youdongsen@163.com>
2016-07-26 10:24:58 +0800

    add java

---
+++ website/database/insertdb.sql
@@ -0,0 +1,14 @@
+INSERT INTO Companys
+(comId,comName,pubTime,heldPlace,msgUrl,srcSchool)
+VALUES
+(1,"华为技术有限公司","2016-04-07","西区活动中心","http://job.ustc.com","中科大");
+
+INSERT INTO Companys
+(comId,comName,pubTime,heldPlace,msgUrl,srcSchool)
+VALUES
+(2,"中兴技术有限公司","2016-04-10","西区活动中心","http://job.ustc.com","中科大");
+
+INSERT INTO Companys
+(comId,comName,pubTime,heldPlace,msgUrl,srcSchool)
+VALUES
+(3,"百度技术有限公司","2016-04-20","东区区活动中心","http://job.ustc.com","东南大学");
